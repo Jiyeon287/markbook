f1cecf896f6261698b08dc72330494ea3da9933a
Jong Seo Ha <hajongse@naver.com>
2021-10-26 15:00:15 +0900

    Initial commit
    초기 세팅

---
+++ markbook/src/test/java/com/markbook/controller/testDataSource.java
@@ -0,0 +1,34 @@
+package com.markbook.controller;
+
+import java.sql.Connection;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+        locations ={"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+
+public class testDataSource {
+   
+    @Inject
+    private DataSource ds;
+   
+    @Test
+    public void testDS() throws Exception{
+       
+        try {
+        	Connection conn = ds.getConnection();
+            System.out.println("success : "+conn);
+            
+        } catch(Exception e){
+            System.out.println("fail : "+e.getMessage());
+        }
+    }
+}
