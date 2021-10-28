3d47886f9b425589363870a0bfb525908ad25c9d
SJ.LEE <92525310+growthlee0125@users.noreply.github.com>
2021-10-28 22:23:52 +0900

    test.sj
    test-sj

--- markbook/src/test/java/com/markbook/controller/testDataSource.java
+++ markbook/src/test/java/com/markbook/controller/testDataSource.java
@@ -27,7 +27,7 @@ public class testDataSource {
         	Connection conn = ds.getConnection();
             System.out.println("success : "+conn);
             System.out.println("hello");
-            System.out.println("¾È³ç");
+            System.out.println("test-sj");
             
         } catch(Exception e){
             System.out.println("fail : "+e.getMessage());
