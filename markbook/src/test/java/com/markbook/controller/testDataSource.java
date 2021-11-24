02aeeaccc7806b7cfec6d6ab45a83c499f44d005
SJ.LEE <92525310+growthlee0125@users.noreply.github.com>
2021-11-24 21:27:59 +0900

    파일업로드 완료, 리스트 목록 구현 완료

--- markbook/src/test/java/com/markbook/controller/testDataSource.java
+++ markbook/src/test/java/com/markbook/controller/testDataSource.java
@@ -7,8 +7,13 @@ import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.markbook.domain.mk_2ndhand_bookVO;
+import com.markbook.persistence.mk_2ndtransDAO;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -16,11 +21,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
         locations ={"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
 
 public class testDataSource {
+	
+	 // 로그
+	private static final Logger logger = LoggerFactory.getLogger(testDataSource.class);
    
     @Inject
     private DataSource ds;
+    
+	@Inject
+	mk_2ndtransDAO mdao;
    
-    @Test
+    //@Test
     public void testDS() throws Exception{
        
         try {
@@ -33,4 +44,23 @@ public class testDataSource {
             System.out.println("fail : "+e.getMessage());
         }
     }
+    
+    // 중고책 데이터 삽입
+	@Test
+	public void book2Create() throws Exception{
+		
+		for (int i=1 ; i<=20 ; i++) {
+			
+			mk_2ndhand_bookVO vo = new mk_2ndhand_bookVO();
+			vo.setB2_num(i+1);
+			vo.setB2_image("다운로드.jpeg");
+			vo.setB2_name("책이름"+i);
+			vo.setB2_writer("작가"+i);
+			
+			mdao.bookRegister(vo);
+		}
+		
+	}
+    
+    
 }
